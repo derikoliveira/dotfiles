28f307121e4f2bf44e47cd89592a83fda4421a16
Derik Oliveira <derikoliveira03@gmail.com>
2025-07-17 00:06:10 -0300

    add vim-tmux-navigator plugin and change keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,17 +1,17 @@
 local utils = require 'config.utils'
 
-vim.keymap.set('n', '<C-k>', function()
+vim.keymap.set('n', '<leader>dO', function()
   utils.delete_blank_line_relative 'above'
 end, { noremap = true, silent = true, desc = 'Delete blank line above' })
 
-vim.keymap.set('n', '<C-j>', function()
+vim.keymap.set('n', '<leader>do', function()
   utils.delete_blank_line_relative 'below'
 end, { noremap = true, silent = true, desc = 'Delete blank line below' })
 
-vim.keymap.set('n', '<A-k>', function()
+vim.keymap.set('n', '<leader>O', function()
   utils.create_blank_line_relative 'above'
 end, { noremap = true, silent = true, desc = 'Insert blank line above' })
 
-vim.keymap.set('n', '<A-j>', function()
+vim.keymap.set('n', '<leader>o', function()
   utils.create_blank_line_relative 'below'
 end, { noremap = true, silent = true, desc = 'Insert blank line below' })
